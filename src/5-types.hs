12dcea2e4a388d36e6aa78e698877e0eb692feaa
Arturas Kveselis <kveselis@gmail.com>
2016-07-05 21:14:14 +0200

    chapter 5 - Types

---
+++ src/5-types.hs
@@ -0,0 +1,20 @@
+module Types where
+
+-- Intermission: Exercises
+
+-- 1.
+-- a) not :: Bool -> Bool
+-- b) length :: [a] -> Int
+-- c) concat :: [[a]] -> [a]
+-- d) head :: [a] -> a
+-- e) (<) :: Ord a => a -> a -> Bool
+
+-- 2.
+-- a) - d)
+-- b) - c)
+-- c) - a)
+-- d) - b)
+-- e) - e)
+
+
+
